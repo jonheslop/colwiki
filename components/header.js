21bf14578f726fc5f85de09749d6b924380aa074
Jonathan Heslop <jonathan.heslop@digitaslbi.com>
2017-03-09 12:19:56 +0000

    Migrating to yarn - so fast :D

--- components/header.js
+++ components/header.js
@@ -4,7 +4,7 @@ import Nav from './nav'
 export default () => (
     <header role="banner" className="fixed vh-100 bg-dark-gray near-white top-0 left-0 w5 flex content-center justify-center flex-column border-box pa4">
         <header className="items-start flex-auto">
-            <h1 className="f1 fw6">Colwiki</h1>
+            <h1 className="f1 lh-title fw6">Untitled Col Database</h1>
             <Nav />
         </header>
         <footer>
