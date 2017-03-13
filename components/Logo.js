f778036585a1dbb4518d2869570d87ccfb9698e3
Jon Heslop <jon@jonheslop.com>
2017-03-13 21:51:46 +0000

    SVG logo

--- components/Logo.js
+++ components/Logo.js
@@ -4,23 +4,11 @@ export default ({ pathname, query }) => (
   <div className="w-100 mb3 cf">
     <Link href="/" prefetch>
       <a>
-        <span className="logo"></span>
-        <span className="logo logo--near-white fr"></span>
+        <svg viewBox="0 0 60 50">
+          <polygon points="50,50 0,50 25,0" style={{fill: '#aaa'}}></polygon>
+          <polygon points="60,50 20,50 40,10" style={{fill: '#f4f4f4'}}></polygon>
+        </svg>
       </a>
     </Link>
-    <style jsx>{`
-      .logo {
-          align-self: center;
-          border-color: transparent transparent #ffffff transparent;
-          border-style: solid;
-          border-width: 0 3em 5em 3em;
-          height: 0;
-          margin-right: 5em;
-          width: 0;
-      }
-      .logo--near-white {
-          border-color: transparent transparent #f4f4f4 transparent;
-      }
-  `}</style>
   </div>
 )
