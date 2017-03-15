21d42e5b9f4d57a2b29307bda3ac29223c1710a6
Jonathan Heslop <jonathan.heslop@digitaslbi.com>
2017-03-15 15:11:49 +0000

    Better spacing

--- components/StravaSegment.js
+++ components/StravaSegment.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import DescriptionTerm from './DescriptionTerm'
 
 export default ({ segment }) => (
-    <div className="pa3 pt0-ns dtc-ns br b--black-20 w5-ns">
+    <aside className="pa3 dtc-ns br b--black-20 w5-ns">
       <DescriptionTerm term="Name" value={ segment.name || '...' } unit="" />
       <DescriptionTerm term="Length" value={ (segment.distance / 1000).toFixed(1) || '...' } unit="km" />
       <DescriptionTerm term="Elevation" value={ segment.total_elevation_gain || '...' } unit="m" />
       <DescriptionTerm term="Avg. Gradient" value={ segment.average_grade || '...' } unit="%" />
       <DescriptionTerm term="Max. Gradient" value={ segment.maximum_grade || '...' } unit="%" />
       <DescriptionTerm term="Category" value={ segment.climb_category == 5 ? 'HC' : segment.climb_category } unit="" />
-      <a className="f6 link orange i" href={`https://www.strava.com/segments/${segment.id}`}>Link to Strava »</a>
-    </div>
+      <a className="lh-copy f6 link orange i" href={`https://www.strava.com/segments/${segment.id}`}>Link to Strava »</a>
+    </aside>
 )
