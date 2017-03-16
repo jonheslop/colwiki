b6a8f1fe76787fdf4f190e2043245499038b6901
Jonathan Heslop <jonathan.heslop@digitaslbi.com>
2017-03-16 13:20:10 +0000

    XO style tweaks

--- components/StravaSegment.js
+++ components/StravaSegment.js
@@ -8,7 +8,7 @@ export default ({segment}) => (
     <DescriptionTerm term="Elevation" value={ segment.total_elevation_gain || '...' } unit="m" />
     <DescriptionTerm term="Avg. Gradient" value={ segment.average_grade || '...' } unit="%" />
     <DescriptionTerm term="Max. Gradient" value={ segment.maximum_grade || '...' } unit="%" />
-    <DescriptionTerm term="Category" value={ segment.climb_category == 5 ? 'HC' : segment.climb_category } unit="" />
+    <DescriptionTerm term="Category" value={ segment.climb_category === 5 ? 'HC' : segment.climb_category } unit="" />
     <a className="lh-copy f6 link orange i" href={`https://www.strava.com/segments/${segment.id}`}>Link to Strava »</a>
   </aside>
 )
