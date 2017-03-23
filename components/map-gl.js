df702907d7109fc589f5464913a40eb7891cfbfa
Jon Heslop <jon@jonheslop.com>
2017-03-23 00:42:21 +0000

    finally got the mapbox GL layers working

--- components/map-gl.js
+++ components/map-gl.js
@@ -13,47 +13,45 @@ export default class extends Component {
   componentWillMount() {
     if (typeof window !== 'undefined') {
       var ReactMapboxGl = require('react-mapbox-gl').default
-      var Layer = ReactMapboxGl.Layer
-      var Feature = ReactMapboxGl.Feature
-      var Marker = ReactMapboxGl.Marker
+      var ReactMapboxGlFunc = require('react-mapbox-gl')
+      var Layer = ReactMapboxGlFunc.Layer
+      var Feature = ReactMapboxGlFunc.Feature
+      var Marker = ReactMapboxGlFunc.Marker
       const polyline = require('@mapbox/polyline')
 
       const routeLine = polyline.decode(this.state.segment.map.polyline)
 
+      routeLine.map(function (coordinates) {
+        return coordinates.reverse()
+      })
+      
       const map = (
         <div className="bt b--black-20">
           <ReactMapboxGl
             style="mapbox://styles/mapbox/outdoors-v9"
             accessToken="pk.eyJ1IjoiY2hyeXNhbGlzc29sbW90aXZlIiwiYSI6ImNqMDB5aG5ndDAwNHUzM3I0cmswbjVvOXYifQ.crgwy6034BHr2ZlLEa5rlg"
-            center={[this.state.segment.end_longitude, this.state.segment.end_latitude]}
+            // fitBounds={[[-79, 43], [-73, 45]]}
+            center={[this.state.segment.start_longitude, this.state.segment.start_latitude]}
             containerStyle={{
-              height: '40vh',
+              height: '50vh',
               width: '100%'
             }}
             >
-            {/* <Layer
-              type="symbol"
-              id="marker"
-              layout={{ "icon-image": "mountain-15" }}
-              paint={{ "icon-color": '#ff4138' }}>
-                <Feature
-                    key={ this.state.segment.id }
-                    coordinates={ [this.state.segment.end_latitude, this.state.segment.end_longitude] } />
-
-            </Layer> */}
-            {/* <Marker
-              coordinates={ [this.state.segment.end_latitude, this.state.segment.end_longitude] }>
-              <h1>Start</h1>
-            </Marker> */}
-            {/* <Marker
-              coordinates={ this.state.end }>
-              <h1>End</h1>
-            </Marker> */}
+            <Marker coordinates={[this.state.segment.start_longitude, this.state.segment.start_latitude]}>
+              <h1 className="green">&bull;</h1>
+            </Marker>
+            <Marker coordinates={[this.state.segment.end_longitude, this.state.segment.end_latitude]}>
+              <h1 className="red">&bull;</h1>
+            </Marker>
             <Layer
               type="line"
               id="line"
               layout={{'line-cap': 'round', 'line-join': 'round'}}
-              paint={{'line-color': '#4790E5', 'line-width': 12}}
+              paint={{
+                'line-color': '#ff4136',
+                'line-opacity': .5,
+                'line-width': 4
+              }}
               >
               <Feature coordinates={routeLine}/>
             </Layer>
