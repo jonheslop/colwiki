97a609e25618bd2cd98ce13b52008cbf1f2d3b5a
Jonathan Heslop <jonathan.heslop@digitaslbi.com>
2017-03-28 17:57:35 +0100

    Better mobile style

--- components/description-term.js
+++ components/description-term.js
@@ -3,9 +3,9 @@ import React from 'react'
 const DescriptionTerm = ({term, value, unit, context}) => {
   if (context === 'single') {
     return (
-      <dl className="db dib-l w-auto-l lh-title mr6-l">
+      <dl className="dib w-auto lh-title mr4 mr5-m mr6-l">
         <dt className="f6 fw4 ml0 gray">{ term }:</dt>
-        <dd className="f2 f1-l fw3 ml0">{ value === 5 ? 'HC' : value || '...' }{ unit }</dd>
+        <dd className="f2 f1-ns fw3 ml0">{ value === 5 ? 'HC' : value || '...' }{ unit }</dd>
       </dl>
     )
   } else {
